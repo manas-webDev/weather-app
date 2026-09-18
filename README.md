807cb82e61b101f5477b60747df170da200735a9
Manas pal <manaspal839@gmail.com>
2026-09-18 13:47:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# 🌤️ Weather App
+
+A responsive weather application built using HTML, CSS and JavaScript. The application allows users to search for a city and view its current weather information using the OpenWeatherMap API.
+
+## ✨ Features
+
+- 🔍 Search weather by city name
+- 🌡️ Display current temperature
+- 🌤️ Display current weather condition
+- 💧 Display humidity
+- 💨 Display wind speed
+- 🌧️ Dynamic weather images
+- ❌ Invalid city error handling
+- ⚡ Real-time weather data using an API
+
+## 🛠️ Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+- OpenWeatherMap API
+
+## 📁 Project Structure
+
+Weather/
+│
+├── index.html
+├── style.css
+├── script.js
+├── README.md
+├── .gitignore
+│
+└── weather-img/
+    └── images/
+        ├── clear.jpg
+        ├── clouds.jpg
+        ├── drizzle.jpg
+        ├── humidity.jpg
+        ├── mist.jpg
+        ├── rain.jpg
+        ├── search.jpg
+        ├── snow.jpg
+        └── wind.jpg
---
+++ script.js
@@ -0,0 +1,45 @@
+const apiKey = "Your_api_key";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+let weatherIcon = document.querySelector(".weather-icon");
+
+let searchBox = document.querySelector(".search input");
+let searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".weather").style.display = "none";
+    document.querySelector(".error").style.display = "block";
+  } else {
+    let data = await response.json();
+    console.log(data);
+
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "weather-img/images/clouds.jpg";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "weather-img/images/clear.jpg";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "weather-img/images/rain.jpg";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "weather-img/images/mist.jpg";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "weather-img/images/drizzle.jpg";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "weather-img/images/snow.jpg";
+    }
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
